76f59bfd537b0335ebecdb6afce707e6b3a843ec
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-04-15 15:06:55 +0200

    Add files via upload

---
+++ RDemo/RDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace RDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var random = new Random();
+            const int passwordlength = 10;
+
+            var buffer = new char[passwordlength];
+              for (var i=0;i <passwordlength;i++)
+              buffer[1] =(char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+        }
+    }
+}
